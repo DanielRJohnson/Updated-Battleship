ad09c9bfc604a04f05980f77ebc80030b459eee9
Daniel Johnson <danielrjohnson00@gmail.com>
2020-09-26 20:27:33 -0500

    Updated documentation

--- Sounds.js
+++ Sounds.js
@@ -2,17 +2,19 @@
 * @description controlling the sound when it should be played or stop
 * @parameter {Space} src The sound sources which should be used
 */
-function sound(src) {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
-    this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
-    this.sound.style.display = "none";
-    document.body.appendChild(this.sound);
-    this.play = function(){
+class sound {
+    constructor(src){
+      this.sound = document.createElement("audio");
+      this.sound.src = src;
+      this.sound.setAttribute("preload", "auto");
+      this.sound.setAttribute("controls", "none");
+      this.sound.style.display = "none";
+      document.body.appendChild(this.sound);
+    }
+    play = function(){
       this.sound.play();
     }
-    this.stop = function(){
+    stop = function(){
       this.sound.pause();
     }
-  }
+}
